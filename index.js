c6b99399d36c3ab61b5b137ccc696c070b14b6d3
byyyyyyyyy <byronbarra09@gmail.com>
2023-08-15 10:14:57 +0800

    updated index file to use cron

--- index.js
+++ index.js
@@ -1,8 +1,2 @@
 import 'dotenv/config';
-import { scrapeDefaultProducts } from './lib/scraper.js';
-import { clearFile } from './lib/utils.js';
-
-// import './lib/cron.js';
-clearFile('listings.json');
-clearFile('shops.json');
-await scrapeDefaultProducts();
+import './lib/cron.js';

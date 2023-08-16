88769fff0e013e7c0ef21de79174ebfe91ab99df
byyyyyyyyy <byronbarra09@gmail.com>
2023-08-16 20:47:13 +0800

    added dynamic revenue filter

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import 'dotenv/config';
 import { scrapeDefaultProducts } from './lib/scraper.js';
-import { clearFile } from './lib/utils.js';
+import { clearFolder } from './lib/utils.js';
 
 // import './lib/cron.js';
-clearFile('listings.json');
-clearFile('shops.json');
+clearFolder('./data');
 await scrapeDefaultProducts();
